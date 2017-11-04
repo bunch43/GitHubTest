21d9ff29ddeda519242a1f7366161dc48ab246bb
Matthew Bunch <matthew.bunch@uconn.edu>
2017-11-03 21:30:25 -0400

    test 2

--- app/src/main/java/com/example/matthewbunch/githubtest/ItemDetailFragment.java
+++ app/src/main/java/com/example/matthewbunch/githubtest/ItemDetailFragment.java
@@ -45,7 +45,7 @@ public class ItemDetailFragment extends Fragment {
             // arguments. In a real-world scenario, use a Loader
             // to load content from a content provider.
             mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
-            
+
             Activity activity = this.getActivity();
             CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
             if (appBarLayout != null) {
---
+++ app/src/main/java/com/example/matthewbunch/githubtest/NewClass.java
@@ -0,0 +1,6 @@
+package com.example.matthewbunch.githubtest;
+
+public class NewClass {
+    public NewClass() {
+    }
+}
